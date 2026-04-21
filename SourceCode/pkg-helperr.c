8a555c3315ca99c3972149791d310be891bd66ad
humrand <oh2063365@gmail.com>
2026-04-21 19:13:37 +0200

    Update application version to 0.0.6-stable

--- SourceCode/pkg-helperr.c
+++ SourceCode/pkg-helperr.c
@@ -12,7 +12,7 @@
 #define MAX_CMD     512
 #define MAX_LINE   1024
 
-#define APP_VERSION "0.0.6-beta"
+#define APP_VERSION "0.0.6-stable"
 
 typedef enum { LANG_ES = 0, LANG_EN = 1 } LangID;
 static LangID g_lang = LANG_EN;
